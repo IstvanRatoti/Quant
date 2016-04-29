636e6ee887d053bd7f96860439c1e43396557bd7
Mr. Rátóti <istvanratoti@gmail.com>
2016-04-29 18:31:15 +0200

    main commit, just a really bare main function

---
+++ Quant/src/quant/main/Main.java
@@ -0,0 +1,11 @@
+package quant.main;
+
+public class Main {
+	
+	public static void main()
+	{
+		// Initialize/Connect to Database first
+		// Pass initial data to the App
+		// Initialize GUI
+	}
+}
