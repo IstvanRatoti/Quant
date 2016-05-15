64a5aa543ebd6255efee798e27f9b1eb6257dd2a
Mr. Rátóti <istvanratoti@gmail.com>
2016-05-15 23:33:51 +0200

    Variables commented out, not useful yet; adaptation to sql data scheme

--- Quant/src/quant/core/Activity.java
+++ Quant/src/quant/core/Activity.java
@@ -5,20 +5,60 @@ import java.util.GregorianCalendar;
 
 public class Activity extends LifeObjective {
 	
-	private String place;
+	private PlaceAndTime placeAndTime;
 	//private int[] travelTime;		unnecessary yet, keep it simple
-	private String link;	//link with other events, TODO: clarify usage, type, etc.
-	private GregorianCalendar[] timeAndDate;
-	private GregorianCalendar dueDate;
+	//private String link; don't bother with it yet	// Link with other events, TODO: clarify usage, type, etc.
+	//private GregorianCalendar dueDate;	Juuust keep it simple
 	/* this variable is for repeated events. Default is 7 because events are usually weekly.*/
-	private int repeated = 7;
+	//private int repeated = 7;		Guess what, keep it simple!
 	
-	public String getPlace() {
-		return place;
+	/* Data structure with this class is more similar to sql database structure.
+	 * Should be easier to connect the two.
+	 */
+	public class PlaceAndTime
+	{
+		private String place;
+		private GregorianCalendar[] timeAndDate;
+		
+		public String getPlace() {
+			return place;
+		}
+		
+		public void setPlace(String place) {
+			this.place = place;
+		}
+		
+		
+		public GregorianCalendar[] getTimeAndDate() {
+			return timeAndDate;
+		}
+		
+		/* 
+		 * Modifies or creates a date and time, requires a starting date and an end date.
+		 * startDate is the array with the start dates, endDate is the end date array
+		 * TODO: testing, exception handling.
+		 */
+		public void setTimeAndDate(GregorianCalendar[] startDate, GregorianCalendar[] endDate) {
+			for(int i=0;i<startDate.length;i++) {
+				this.timeAndDate[i*2]=startDate[i];
+				this.timeAndDate[i*2+1]=endDate[i];
+			}
+		}
+		
+		/*
+		 * For Unscheduled activities.
+		 */
+		public void setTimeAndDate() {
+			this.timeAndDate=null;
+		}
 	}
-	
-	public void setPlace(String place) {
-		this.place = place;
+
+	public PlaceAndTime getPlaceAndTime() {
+		return placeAndTime;
+	}
+
+	public void setPlaceAndTime(PlaceAndTime placeAndTime) {
+		this.placeAndTime = placeAndTime;
 	}
 
 	/*public int[] getTravelTime() {
@@ -28,7 +68,7 @@ public class Activity extends LifeObjective {
 	public void setTravelTime(int hours, int minutes) {
 		this.travelTime[0] = hours;
 		this.travelTime[1] = minutes;
-	}*/
+	}
 
 	public String getLink() {
 		return link;
@@ -36,32 +76,9 @@ public class Activity extends LifeObjective {
 
 	public void setLink(String link) {
 		this.link = link;
-	}
-
-	public GregorianCalendar[] getTimeAndDate() {
-		return timeAndDate;
-	}
+	}*/
 	
-	/* 
-	 * Modifies or creates a date and time, requires a starting date and an end date.
-	 * startDate is the array with the start dates, endDate is the end date array
-	 * TODO: testing, exception handling.
-	 */
-	public void setTimeAndDate(GregorianCalendar[] startDate, GregorianCalendar[] endDate) {
-		for(int i=0;i<startDate.length;i++) {
-			this.timeAndDate[i*2]=startDate[i];
-			this.timeAndDate[i*2+1]=endDate[i];
-		}
-	}
-	
-	/*
-	 * For Unscheduled activities.
-	 */
-	public void setTimeAndDate() {
-		this.timeAndDate=null;
-	}
-	
-	public int getRepeated() {
+	/*public int getRepeated() {
 		return repeated;
 	}
 
@@ -75,5 +92,5 @@ public class Activity extends LifeObjective {
 
 	public void setDueDate(GregorianCalendar dueDate) {
 		this.dueDate = dueDate;
-	}
+	}*/
 }
